39f137f53fd71a6158e152c52565b87900eb1e67
William <williamgranath@gmail.com>
2022-01-14 19:29:50 +0100

    URL

--- src/login-to-spotify.js
+++ src/login-to-spotify.js
@@ -2,7 +2,7 @@
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = '5c6fee27ff8b49e8bf8b0be5bbdfa299';
-const redirectUri = 'https://spotifyclone-wille.netlify.app/';
+const redirectUri = 'https://spotifyclone-wille.netlify.app';
 const scopes = [
 	'user-read-currently-playing',
 	'user-read-recently-played',
